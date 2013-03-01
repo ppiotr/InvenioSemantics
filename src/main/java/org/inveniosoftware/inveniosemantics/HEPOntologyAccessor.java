0bac08053c1fac447d1bfc0b55f599900c82e155
Piotr Praczyk <piotr.praczyk@gmail.com>
2013-03-01 19:51:20 +0100

    Extended the ontology (Now it allows to store information about cootdinate systems) and added appropriate accessors

--- src/main/java/org/inveniosoftware/inveniosemantics/HEPOntologyAccessor.java
+++ src/main/java/org/inveniosoftware/inveniosemantics/HEPOntologyAccessor.java
@@ -21,170 +21,189 @@ import com.hp.hpl.jena.util.FileManager;
 import java.io.*;
 import java.util.*;
 
-/** Class that enables the access to the HEP ontology */
+/**
+ * Class that enables the access to the HEP ontology
+ */
 public class HEPOntologyAccessor {
-	
-	private String _inputFileName;
-	
-	private Model _model;
-	
-	private static final String NARROWER = "narrower";
-	private static final String PREFLABEL = "prefLabel";
-	
-	public static final String NSPREFIX ="hep";
-	
-	public static final String NSURI ="http://cern.ch/thesauri/HEPontology.rdf#";
 
-	/** Constructor that receives the path of the file containing the HEP ontology */
-	public HEPOntologyAccessor(String inputFileName) {
-	  _inputFileName = inputFileName;
-	  uploadModel();
-	}
+    private String _inputFileName;
+    private Model _model;
+    private static final String NARROWER = "narrower";
+    private static final String PREFLABEL = "prefLabel";
+    public static final String NSPREFIX = "hep";
+    public static final String NSURI = "http://cern.ch/thesauri/HEPontology.rdf#";
 
-    /** It uploads the model in memory. This is not efficient but it works for a test. For production, a TDB or a database storage should be used instead */
-	private void uploadModel() {
-		 InputStream in = FileManager.get().open( _inputFileName );
-		 if (in == null) {
-		   throw new IllegalArgumentException( "File: " + _inputFileName + " not found");
-		 }
-		 _model = ModelFactory.createDefaultModel();
-		 // read the RDF/XML file
-		 _model.read(in, "");
-	
-	}
-	
-	/** This is a test method to show the the first 'limit' statements (triples) of the model
-	 * @param limit maximum number of statements to be displayed. -1 if you want to display all the statements
-	 */
-	public void showStatements( int limit) {
-		StmtIterator iter = _model.listStatements();
-		int i=0;
-  	    while (iter.hasNext()&& ((i<limit)||(limit==-1))) {
-		     Statement st = iter.next();
-		     System.out.println("  " + st.toString());
-		     i++;
-	   }
-	}
-	
-	/** This is a test method, to show the the first 'limit' statements (triples) of the model. It's a refined version of showStatements method
-	 * @param limit maximum number of statements to be displayed. -1 if you want to display all the statements
-	 */
-	public void showStatementsRefined(int limit) {
-  	  // list the statements in the graph
-  	  StmtIterator iter = _model.listStatements();
-  	        
-  	  // print out the predicate, subject and object of each statement
-		int i=0;
-  	    while (iter.hasNext()&& ((i<limit)||(limit==-1))) {
-  	    Statement stmt      = iter.nextStatement(); // get next statement
-  	    Resource  subject   = stmt.getSubject();   // get the subject
-  	    Property  predicate = stmt.getPredicate(); // get the predicate
-  	    RDFNode   object    = stmt.getObject();    // get the object
-  	            
-  	    System.out.print(subject.toString());
-  	    System.out.print(" " + predicate.toString() + " ");
-  	    if (object instanceof Resource) {
-  	      System.out.print(object.toString());
-  	    } else {
-  	      // object is a literal
-  	      System.out.print(" \"" + object.toString() + "\"");
-  	    }
-  	    System.out.println(" .");
-	     i++;
-  	  }
-  	  
-	}
+    /**
+     * Constructor that receives the path of the file containing the HEP
+     * ontology
+     */
+    public HEPOntologyAccessor(String inputFileName) {
+        _inputFileName = inputFileName;
+        uploadModel();
+    }
 
-	/** This is a recursive auxiliary method used to display the recursive hierarchy of narrower terms of a concept
-	 * @param r resource to be browsed in each invocation
-	 * @param index number of commas to be shown in the output (it allows detecting the hierarchy)
-	 */
-	private static void showTaxonomy(Resource r, int index){
-		  for (int i=0;i<index;i++)
-	        System.out.print(",");
-		  System.out.println(r.toString());
-		  
-	  	  // list the statements in the graph
-	  	  StmtIterator iter = r.listProperties();
-	  	        
-	  	  // print out the predicate, subject and object of each statement
-	  	  while (iter.hasNext()) {
-	  	    Statement stmt      = iter.nextStatement(); // get next statement
-	  	    // Resource  subject   = stmt.getSubject();   // get the subject
-	  	    Property  predicate = stmt.getPredicate(); // get the predicate
-	  	    RDFNode   object    = stmt.getObject();    // get the object
-	  	            
-	  	    if (predicate.getLocalName().contains(NARROWER)) {
-		  	    if (object instanceof Resource) {
-			  	      showTaxonomy((Resource)object,index+1);
-		  	    }
-	  	    }
-	  	  }
-	}
-	
-	
-	/** This method displays the recursive hierarchy of narrower terms of 'concept'. It uses a selector to filter the statements of the model.
-	 * @param r resource to be browsed in each invocation
-	 * @param index number of commas to be shown in the output (it allows detecting the hierarchy)
-	 */
-	public void extractTaxonomy(final String concept) {
-	  	  // list the statements in the graph
-	  	  StmtIterator iter = _model.listStatements(
-	              new 
-	                SimpleSelector(null, null, (RDFNode) null) {
-	                    @Override
-	                    public boolean selects(Statement s) {
-	                    	Resource subject = s.getSubject();
-	                        // return subject.getURI().endsWith("#matter");
-	                        return subject.getURI().endsWith("#"+concept);
-	                    }
-	                });
+    /**
+     * It uploads the model in memory. This is not efficient but it works for a
+     * test. For production, a TDB or a database storage should be used instead
+     */
+    private void uploadModel() {
+        InputStream in = FileManager.get().open(_inputFileName);
+        if (in == null) {
+            throw new IllegalArgumentException("File: " + _inputFileName + " not found");
+        }
+        _model = ModelFactory.createDefaultModel();
+        // read the RDF/XML file
+        _model.read(in, "");
 
-	  	  
-	  	  // print out the predicate, subject and object of each statement
-	  	  while (iter.hasNext()) {
-	  	    Statement stmt      = iter.nextStatement(); // get next statement
-	  	    Resource  subject   = stmt.getSubject();   // get the subject
-	  	    Property  predicate = stmt.getPredicate(); // get the predicate
-	  	    //RDFNode   object    = stmt.getObject();    // get the object
+    }
 
-	  	    if (predicate.toString().contains(PREFLABEL)) {
-	  	    	// found prefLabel 
-	  	    	showTaxonomy(subject,0);
-	  	    }
-	  	    	
-	  	  }
-	  	  
-		}
-	
-	
+    /**
+     * This is a test method to show the the first 'limit' statements (triples)
+     * of the model
+     *
+     * @param limit maximum number of statements to be displayed. -1 if you want
+     * to display all the statements
+     */
+    public void showStatements(int limit) {
+        StmtIterator iter = _model.listStatements();
+        int i = 0;
+        while (iter.hasNext() && ((i < limit) || (limit == -1))) {
+            Statement st = iter.next();
+            System.out.println("  " + st.toString());
+            i++;
+        }
+    }
+
+    /**
+     * This is a test method, to show the the first 'limit' statements (triples)
+     * of the model. It's a refined version of showStatements method
+     *
+     * @param limit maximum number of statements to be displayed. -1 if you want
+     * to display all the statements
+     */
+    public void showStatementsRefined(int limit) {
+        // list the statements in the graph
+        StmtIterator iter = _model.listStatements();
+
+        // print out the predicate, subject and object of each statement
+        int i = 0;
+        while (iter.hasNext() && ((i < limit) || (limit == -1))) {
+            Statement stmt = iter.nextStatement(); // get next statement
+            Resource subject = stmt.getSubject();   // get the subject
+            Property predicate = stmt.getPredicate(); // get the predicate
+            RDFNode object = stmt.getObject();    // get the object
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+            System.out.println(" .");
+            i++;
+        }
+
+    }
+
+    /**
+     * This is a recursive auxiliary method used to display the recursive
+     * hierarchy of narrower terms of a concept
+     *
+     * @param r resource to be browsed in each invocation
+     * @param index number of commas to be shown in the output (it allows
+     * detecting the hierarchy)
+     */
+    private static void showTaxonomy(Resource r, int index) {
+        for (int i = 0; i < index; i++) {
+            System.out.print(",");
+        }
+        System.out.println(r.toString());
+
+        // list the statements in the graph
+        StmtIterator iter = r.listProperties();
+
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt = iter.nextStatement(); // get next statement
+            // Resource  subject   = stmt.getSubject();   // get the subject
+            Property predicate = stmt.getPredicate(); // get the predicate
+            RDFNode object = stmt.getObject();    // get the object
+
+            if (predicate.getLocalName().contains(NARROWER)) {
+                if (object instanceof Resource) {
+                    showTaxonomy((Resource) object, index + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * This method displays the recursive hierarchy of narrower terms of
+     * 'concept'. It uses a selector to filter the statements of the model.
+     *
+     * @param r resource to be browsed in each invocation
+     * @param index number of commas to be shown in the output (it allows
+     * detecting the hierarchy)
+     */
+    public void extractTaxonomy(final String concept) {
+        // list the statements in the graph
+        StmtIterator iter = _model.listStatements(
+                new SimpleSelector(null, null, (RDFNode) null) {
+                    @Override
+                    public boolean selects(Statement s) {
+                        Resource subject = s.getSubject();
+                        // return subject.getURI().endsWith("#matter");
+                        return subject.getURI().endsWith("#" + concept);
+                    }
+                });
 
 
-    /** This method search all resources containing concept as preferred label or alternate label
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt = iter.nextStatement(); // get next statement
+            Resource subject = stmt.getSubject();   // get the subject
+            Property predicate = stmt.getPredicate(); // get the predicate
+            //RDFNode   object    = stmt.getObject();    // get the object
+
+            if (predicate.toString().contains(PREFLABEL)) {
+                // found prefLabel 
+                showTaxonomy(subject, 0);
+            }
+
+        }
+
+    }
+
+    /**
+     * This method search all resources containing concept as preferred label or
+     * alternate label
+     *
      * @param concept literal of the concept to be searched
      */
     public List<Resource> searchResources(String concept) {
         try {
 
-        	// the query is upper or lower case independent
-        	// the prefLabel or altLabel should be the same as concept
-        	// the query could be modified according to user needs for more flexibility
-        	String query = 
-                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
-                    "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
-                    "SELECT DISTINCT ?subject " +
-                    "WHERE { " +
-                    "{ ?subject skos:prefLabel ?object . " +
-                    "FILTER regex(str(?object),'" + "^" + concept + "$','i') }" +
-                    "UNION " +
-                    "{?subject skos:altLabel ?object . " +
-                    "FILTER regex(str(?object),'" + "^" + concept + "$','i') }" +
-                    "} ORDER BY ASC(?subject) ";
-        			
+            // the query is upper or lower case independent
+            // the prefLabel or altLabel should be the same as concept
+            // the query could be modified according to user needs for more flexibility
+            String query =
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                    + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                    + "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> "
+                    + "SELECT DISTINCT ?subject "
+                    + "WHERE { "
+                    + "{ ?subject skos:prefLabel ?object . "
+                    + "FILTER regex(str(?object),'" + "^" + concept + "$','i') }"
+                    + "UNION "
+                    + "{?subject skos:altLabel ?object . "
+                    + "FILTER regex(str(?object),'" + "^" + concept + "$','i') }"
+                    + "} ORDER BY ASC(?subject) ";
 
-        	// System.out.println(query);
-        	
+
+            // System.out.println(query);
+
             QueryExecution qexec = QueryExecutionFactory.create(query, _model);
             ResultSet results = qexec.execSelect();
 
@@ -219,35 +238,33 @@ public class HEPOntologyAccessor {
         }
         return true;
     }
-    
-    
 
-    public static void main(String[] args){
-		System.out.println("Starting");
+    public static void main(String[] args) {
+        System.out.println("Starting");
 
-		String inputFileName  = "files/HEPont.rdf";
-		HEPOntologyAccessor hep = new HEPOntologyAccessor(inputFileName);
+        String inputFileName = "files/HEPont.rdf";
+        HEPOntologyAccessor hep = new HEPOntologyAccessor(inputFileName);
 
-		System.out.println("\nTEST: SHOW STATEMENTS");
-		hep.showStatements( 20);
-		
-		System.out.println("\nTEST: SHOW STATEMENTS IN REFINED FORM");
-		hep.showStatementsRefined( 20);
+        System.out.println("\nTEST: SHOW STATEMENTS");
+        hep.showStatements(20);
 
-		System.out.println("\nTEST: SHOW HIERARCHY OF CONCEPTS USING "+NARROWER+" RELATIONS");
-		hep.extractTaxonomy("matter");
-		
+        System.out.println("\nTEST: SHOW STATEMENTS IN REFINED FORM");
+        hep.showStatementsRefined(20);
 
-		System.out.println("\nTEST: SEARCH CONCEPTS");
+        System.out.println("\nTEST: SHOW HIERARCHY OF CONCEPTS USING " + NARROWER + " RELATIONS");
+        hep.extractTaxonomy("matter");
 
 
-		List<Resource> results = hep.searchResources("particle");
-		
-		for (Resource r: results) {
-			System.out.println(r);
-		}
+        System.out.println("\nTEST: SEARCH CONCEPTS");
 
-		System.out.println("Finished");
+
+        List<Resource> results = hep.searchResources("particle");
+
+        for (Resource r : results) {
+            System.out.println(r);
+        }
+
+        System.out.println("Finished");
 
     }
 }
--- src/main/java/org/inveniosoftware/inveniosemantics/InvenioOntologyAccessor.java
+++ src/main/java/org/inveniosoftware/inveniosemantics/InvenioOntologyAccessor.java
@@ -1,8 +1,12 @@
 package org.inveniosoftware.inveniosemantics;
 
 import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.rdf.model.impl.StatementImpl;
 import com.hp.hpl.jena.util.FileManager;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class InvenioOntologyAccessor {
 
@@ -24,12 +28,65 @@ public class InvenioOntologyAccessor {
     public static final String PLOT = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#Plot";
     public static final String PUBLICATION = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#Publication";
     public static final String TABLE = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#Table";
+    public static final String COORDINATE_SYSTEM = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#CoordinateSystem";
+    public static final String CONTAINS = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#contains";
+    public static final String AXIS = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#Axis";
+    public static final String AXIS_TICK = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#AxisTick";
+    public static final String AXIS_LABEL = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#AxisLabel";
+    public static final String AXIS_TICK_LABEL = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#AxisTickLabel";
+    public static final String COORDINATE_SYSTEM_ORIGIN = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#CoordinateSystemOrigin";
+    public static final String IS_LOCATED_AT = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#isLocatedAt";
+    public static final String HAS_X_COORDINATE = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#hasXCoordinate";
+    public static final String HAS_Y_COORDINATE = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#hasYCoordinate";
+    public static final String HAS_WIDTH = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#hasWidth";
+    public static final String HAS_HEIGHT = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#hasHeight";
+    public static final String HAS_BOUNDARY = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#hasBoundary";
     public static final String NSPREFIX = "inveniomodel";
     public static final String NSURI = "http://www.semanticweb.org/ontologies/invenio/inveniomodel.owl#";
+    public Resource coordinateSystem;
+    public Resource plot;
+    public Resource axis;
+    public Resource coordinateSystemOrigin;
+    public Resource axisTick;
+    public Resource axisTickLabel;
+    public Resource axisLabel;
+    public Property isLocatedAt;
+    public Property hasBoundary;
+    public Property contains;
+    public Property hasXCoordinate;
+    public Property hasYCoordinate;
+    public Property hasWidth;
+    public Property hasHeight;
 
     public InvenioOntologyAccessor(String inputFileName) {
         _inputFileName = inputFileName;
         uploadModel();
+        createResources();
+    }
+
+    /**
+     * loads resources of the ontology and assigns them to object fields
+     */
+    private void createResources() {
+        this.coordinateSystem = this._model.createResource(InvenioOntologyAccessor.COORDINATE_SYSTEM);
+        this.coordinateSystemOrigin = this._model.createResource(InvenioOntologyAccessor.COORDINATE_SYSTEM_ORIGIN);
+        this.axisTick = this._model.createResource(InvenioOntologyAccessor.AXIS_TICK);
+        this.axisTickLabel = this._model.createResource(InvenioOntologyAccessor.AXIS_TICK_LABEL);
+        this.axisLabel = this._model.createResource(InvenioOntologyAccessor.AXIS_LABEL);
+
+
+        this.plot = this._model.createResource(InvenioOntologyAccessor.PLOT);
+        this.axis = this._model.createResource(InvenioOntologyAccessor.AXIS);
+
+        this.contains = this._model.createProperty(InvenioOntologyAccessor.CONTAINS);
+        this.isLocatedAt = this._model.createProperty(InvenioOntologyAccessor.IS_LOCATED_AT);
+        this.hasBoundary = this._model.createProperty(InvenioOntologyAccessor.HAS_BOUNDARY);
+
+        this.hasXCoordinate = this._model.createProperty(InvenioOntologyAccessor.HAS_X_COORDINATE);
+        this.hasYCoordinate = this._model.createProperty(InvenioOntologyAccessor.HAS_Y_COORDINATE);
+        this.hasWidth = this._model.createProperty(InvenioOntologyAccessor.HAS_WIDTH);
+        this.hasHeight = this._model.createProperty(InvenioOntologyAccessor.HAS_HEIGHT);
+
     }
 
     private void uploadModel() {
@@ -41,6 +98,11 @@ public class InvenioOntologyAccessor {
         // read the RDF/XML file
         _model.read(in, "");
 
+    }
+
+    public void saveModel() throws FileNotFoundException {
+//        OutputStream os = new FileOutputStream("../InvenioSemantics/files/invenio.rdf");
+        _model.write(System.out);
     }
 
     /**
@@ -63,7 +125,10 @@ public class InvenioOntologyAccessor {
     public static void main(String[] args) {
 
         InvenioOntologyAccessor invenio = new InvenioOntologyAccessor("files/inveniomodel.owl");
-
         invenio.showStatements(-1);
+    }
+
+    public Model getModel() {
+        return this._model;
     }
 }
